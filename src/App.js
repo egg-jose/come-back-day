d7009d2c867b65ad20871220638ebe6816041265
Jose Esteban Garcia Grullon <josegg1897@gmail.com>
2020-01-07 00:26:10 -0400

    LAST CHANGES >:V

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css'
 import React, { Component } from 'react'
+import mario from './images/mario.jpg'
 
 export default class App extends Component {
   state = {
@@ -8,6 +9,26 @@ export default class App extends Component {
     minutes: 0,
     seconds: 0,
     end: false
+  }
+  componentDidMount() {
+    var countDownDate = new Date("Jan 7, 2020 7:30:0").getTime();
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24)).toString();
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)).toString();
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)).toString();
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000).toString();
+    this.setState({
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds
+    })
+    if (distance < 0) {
+      this.setState({
+        end: true
+      })
+    }
   }
   cTime() {
     setInterval(() => {
@@ -35,71 +56,54 @@ export default class App extends Component {
     }, 1000)
   }
   render() {
-    this.cTime();
-    const { days, hours, minutes, seconds } = this.state
-    return (
-      // <div className="clock">
-      //   <div className="numbers">
-      //     <p className="hours">{days}</p>
-      //     <p className="placeholder">88</p>
-      //   </div>
 
-      //   <div className="colon">
-      //     <p>:</p>
-      //   </div>
-      //   <div className="numbers">
-      //     <p className="hours">{hours.length >= 2 ? hours : '0'+ hours}</p>
-      //     <p className="placeholder">88</p>
-      //   </div>
+    const { days, hours, minutes, seconds, end } = this.state
 
-      //   <div className="colon">
-      //     <p>:</p>
-      //   </div>
-
-      //   <div className="numbers">
-      //     <p className="minutes">{minutes.length >= 2 ? minutes : '0'+ minutes}</p>
-      //     <p className="placeholder">88</p>
-      //   </div>
-
-      //   <div className="colon">
-      //     <p>:</p>
-      //   </div>
-
-      //   <div className="numbers">
-      //     <p className="seconds">{seconds.length >= 2 ? seconds : '0'+ seconds}</p>
-      //     <p className="placeholder">88</p>
-      //   </div>
-      // </div>
-      <div className="clock">
-        <div className="numbers">
-          <p className="hours">{days}&nbsp;</p>
-          <p className="placeholder">88</p>
-        </div>
-        Dias
+    if (!end) {
+      this.cTime();
+      return (
+        <div className="clock">
+          <div className="numbers">
+            <p className="hours">{days}&nbsp;</p>
+            <p className="placeholder">88</p>
+          </div>
+          Dias
         <br />
+          <br />
+          <div className="numbers">
+            <p className="hours">{hours.length >= 2 ? hours : '0' + hours}&nbsp;</p>
+            <p className="placeholder">88</p>
+          </div>
+          Horas
         <br />
-        <div className="numbers">
-          <p className="hours">{hours.length >= 2 ? hours : '0' + hours}&nbsp;</p>
-          <p className="placeholder">88</p>
-        </div>
-        Horas
-        <br />
-        <br />
+          <br />
 
-        <div className="numbers">
-          <p className="minutes">{minutes.length >= 2 ? minutes : '0' + minutes}&nbsp;</p>
-          <p className="placeholder">88</p>
-        </div>
-        Minutos
+          <div className="numbers">
+            <p className="minutes">{minutes.length >= 2 ? minutes : '0' + minutes}&nbsp;</p>
+            <p className="placeholder">88</p>
+          </div>
+          Minutos
         <br />
-        <br />
+          <br />
 
-        <div className="numbers">
-          <p className="seconds">{seconds.length >= 2 ? seconds : '0' + seconds}&nbsp;</p>
-          <p className="placeholder">88</p>
-        </div>
-        Segundos
+          <div className="numbers">
+            <p className="seconds">{seconds.length >= 2 ? seconds : '0' + seconds}&nbsp;</p>
+            <p className="placeholder">88</p>
+          </div>
+          Segundos
       </div>
-    )
+      )
+    }
+    else {
+      return (
+        <React.Fragment>
+          <img src={mario} alt={"mario"} />
+          <div className="container">
+            <h1>HERE WE GOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOoOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO</h1>
+          </div>
+        </React.Fragment>
+
+      );
+    }
   }
 }
