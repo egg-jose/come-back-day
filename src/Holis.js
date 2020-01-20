b6d4c0638c1844da41358f956a712abd65ad69f4
Jose <josegg1897@gmail.com>
2020-01-19 23:33:16 -0400

    Nel perro

--- src/Holis.js
+++ src/Holis.js
@@ -6,7 +6,10 @@ export default class Holis extends Component {
     render() {
         return (
             <div className="bottom">
-                <h1>Holis :3</h1>
+                <h1>innerWidth: {window.innerWidth}</h1>
+                <h1>innerHeigh: {window.innerHeigh}</h1>
+                <h1>outerWidth: {window.outerWidth}</h1>
+                <h1>outerHeight: {window.outerHeight}</h1>
             </div>
         )
     }
